ec1548d59bd1461dfb5a7dbb491a17ca763d5de8
Oleksandr Vasianovych <derdef360@gmail.com>
2022-11-03 14:21:16 +0200

    Sanaprakt1 second commit

--- Task1/Task1.cpp
+++ Task1/Task1.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Sana Commerce Software Development Bootcamp\n";
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
